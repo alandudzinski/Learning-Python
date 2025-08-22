83bea2a252691422a93b6c4b1b2e0c1a56fab868
Cub0p <adudzinski177@gmail.com>
2025-08-22 12:34:04 -0400

    Change repo structure

---
+++ Week2/Day8/requests.py
@@ -0,0 +1,10 @@
+import requests
+
+response = requests.get("https://official-joke-api.appspot.com/random_joke")
+
+if response.status_code == 200:
+    joke = response.json()
+    print(joke["setup"])
+    print(joke["punchline"])
+else:
+    print("Error:", response.status_code)
